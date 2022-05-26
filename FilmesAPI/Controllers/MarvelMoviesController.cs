50c2662430f18b7a45abad7fa976c4ddc6263891
reppl <reppl@THENEGRO>
2022-05-26 20:11:36 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/MarvelMoviesController.cs
@@ -0,0 +1,39 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+
+    public class MarvelMoviesController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+
+        [HttpPost]
+
+        public void AddMovie([FromBody] Movie movie)
+        {
+
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+
+        }
+
+        [HttpGet]
+
+        public IEnumerable<Movie> RecoverMovies()
+        {
+            return movies;
+        }
+
+
+
+
+
+
+    }
+    
+}
---
+++ FilmesAPI/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "The field title is mandatory") ]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The field Director is mandatory")]
+        public string Director { get; set; }
+
+        [StringLength(30, ErrorMessage = "The gender can't be above 30 characters")]
+        public string Gender { get; set; }
+
+        public string Date { get; set; }
+
+        [Range(1,200,ErrorMessage = "The time of the movie can't be above 200 minutes ")]
+        public int time { get; set; }
+
+       
+
+
+
+    }
+}
